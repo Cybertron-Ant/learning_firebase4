9a22a57ca3b73066070214cf136b6861ba057f62
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 14:49:32 -0500

    grab a reference to the 'players' collection and call the 'child' method on it

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -4,3 +4,7 @@
 
 //grab a reference to 'players', which will be used as the name to create a collection later
 var ref = db.ref('players');
+
+
+//grab a reference to the 'players' collection and call the 'child' method on it
+var playersRef = ref.child('players');
