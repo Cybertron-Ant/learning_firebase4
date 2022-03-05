a3f2083b8d3ec2bf211ca62c754b979f903108ec
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 14:36:43 -0500

    I can update the Firebase data. Notice how I amusing the 'players/john' path

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -4,3 +4,7 @@
 
 //grab a reference to 'players', which will be used as the name to create a collection later
 var ref = db.ref('players');
+
+
+// I can update the Firebase data. Notice how I amusing the 'players/john' path
+var johnRef = db.ref("players/John");
