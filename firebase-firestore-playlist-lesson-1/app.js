5c694c6489bbc7484bb6662c20c4c2f69af82f5b
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 14:29:29 -0500

    The 'set' method will write or replace data on a specified path. create a reference to the "player’s" collection and set two players

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -4,3 +4,16 @@
 
 //grab a reference to 'players', which will be used as the name to create a collection later
 var ref = db.ref('players');
+
+//The 'set' method will write or replace data on a specified path. create a reference to the "player’s" collection and set two players
+ref.set({
+  John: {
+    number: 1,
+    age: 30
+  },
+
+  Amanda: {
+    number: 2,
+    age: 20
+  }
+});
