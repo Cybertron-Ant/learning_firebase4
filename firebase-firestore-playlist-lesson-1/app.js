bb4cc7579d597cdadd0d8fbdaf0fe7d0ae7a4414
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 15:35:16 -0500

    use the on() method to retrieve data. This method is taking the event type as "value" and then retrieves the snapshot of the data. add the val() method to the snapshot to gets its value

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -8,3 +8,15 @@ var ref = db.ref('players');
 
 //grab a reference to the 'players' that is a child of(nested inside) the 'players' collection
 var ref = db.ref('/players/players');
+
+
+//use the on() method to retrieve data. This method is taking the event type as "value" and then retrieves the snapshot of the data. add the val() method to the snapshot to gets its value 
+ref.on("value", function(snapshot) {
+  
+  console.log(snapshot.val());
+  
+}, function(error) {
+  
+  console.log("Error: " + error.code);
+  
+});//end error callback function
