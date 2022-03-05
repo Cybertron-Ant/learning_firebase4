27791b632ac9f9e18aff62e1583375ba93391d82
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 18:52:14 -0500

    I must manually hange "Bob" to "Maria" in Firebase to get the update. 'child_changed' event type is triggered when the data has changed

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -8,3 +8,14 @@ var ref = db.ref('players');
 
 //grab a reference to the 'players' that is a child of(nested inside) the 'players' collection
 var playersRef = db.ref('/players/players');
+
+
+//I must manually hange "Bob" to "Maria" in Firebase to get the update. 'child_changed' event type is triggered when the data has changed
+playersRef.on("child_changed", function(data) {
+  
+  //access the value of the 'data' object using the 'val()' method
+  var player = data.val();
+  
+  console.log("The updated player name is " + player.name);
+  
+});
