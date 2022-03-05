9e859f636dea09eb54c8f830f067c2ee6fbbbc73
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 14:54:13 -0500

    the 'child' method will nest the 'players' inside the 'players' collection as a child of it

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -7,4 +7,5 @@ var ref = db.ref('players');
 
 
 //grab a reference to the 'players' collection and call the 'child' method on it
+//the 'child' method will nest the 'players' inside the 'players' collection as a child of it
 var playersRef = ref.child('players');
