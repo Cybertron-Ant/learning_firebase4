fdb1049a4baa030606748718d8cfcf1027799878
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 15:21:58 -0500

    I can get any key from Firebase by using the key() method. if I want to get the collection name

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -8,3 +8,9 @@ var ref = db.ref('players');
 
 //grab a reference to the 'players' that is a child of(nested inside) 'players', using the 'child' method
 var playersRef = ref.child("players");
+
+
+// I can get any key from Firebase by using the key() method. if I want to get the collection name
+var playersKey = playersRef.key();
+
+console.log(playersKey);
