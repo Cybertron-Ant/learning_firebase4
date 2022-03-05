2a1b7bc2f7a6929bac279d8195139c2e38ccbe1c
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 15:05:40 -0500

    grab a reference to the 'age' of 'Amanda' from the collection

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -4,3 +4,7 @@
 
 //grab a reference to 'players', which will be used as the name to create a collection later
 var ref = db.ref('players');
+
+
+//grab a reference to the 'age' of 'Amanda' from the collection
+ var amandaAgeRef = ref.child("players").child("-MxQcXJBDbSbE5hTc89O").child('age');console.log(amandaAgeRef)
