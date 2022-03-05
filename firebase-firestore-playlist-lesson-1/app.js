da570cf20bccc973f95a8e14ec122bbcaf9b02c7
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 15:10:08 -0500

    use the 'transaction' method, that takes a function as its argument to return the current age, add one year to it, and update the collection

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -7,4 +7,11 @@ var ref = db.ref('players');
 
 
 //grab a reference to the 'age' of 'Amanda' from the collection
- var amandaAgeRef = ref.child("players").child("-MxQcXJBDbSbE5hTc89O").child('age');console.log(amandaAgeRef)
+ var amandaAgeRef = ref.child("players").child("-MxQcXJBDbSbE5hTc89O").child('age');
+ 
+ 
+// use the 'transaction' method, that takes a function as its argument to return the current age, add one year to it, and update the collection
+amandaAgeRef.transaction(function(currentAge) {
+   return currentAge + 1;
+});
+
