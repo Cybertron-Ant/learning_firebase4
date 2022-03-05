b57c9e22329d22f5777751fcda11ddf480f78fdb
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 14:40:59 -0500

    call the 'update' method and set the 'number' key to a new value

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -8,3 +8,9 @@ var ref = db.ref('players');
 
 // I can update the Firebase data. Notice how I amusing the 'players/john' path
 var johnRef = db.ref("players/John");
+
+
+//call the 'update' method and set the 'number' key to a new value
+johnRef.update ({
+   "number": 10
+});
