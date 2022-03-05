25e656308f64b14f90148cc54c90e591a5e0be48
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 14:55:28 -0500

    The push() method will create a unique id when the data is pushed. If I want to create my players with a unique id, under the nested 'players'

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -9,3 +9,17 @@ var ref = db.ref('players');
 //grab a reference to the 'players' collection and call the 'child' method on it
 //the 'child' method will nest the 'players' inside the 'players' collection as a child of it
 var playersRef = ref.child('players');
+
+
+//The push() method will create a unique id when the data is pushed. If I want to create my players with a unique id, under the nested 'players'
+playersRef.push({
+  name: "John",
+  number: 1,
+  age: 30
+});
+
+playersRef.push({
+  name: "Amanda",
+  number: 2,
+  age: 20
+});
