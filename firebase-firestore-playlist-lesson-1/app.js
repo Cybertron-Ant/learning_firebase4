a197ad153060ecb34bb46da0bbcdba88db24a3f5
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 21:00:18 -0500

    use the on() method to retrieve data. This method is taking the event type as "value" and then retrieves the snapshot of the data

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -8,3 +8,16 @@ var ref = db.ref('players');
 
 //grab a reference to the last player in the 'players' that is a child of(nested inside) the 'players' collection
 var lastPlayerRef = firebase.database().ref("/players/players").limitToLast(1);
+
+
+//use the on() method to retrieve data. This method is taking the event type as "value" and then retrieves the snapshot of the data
+lastPlayerRef.on("value", function(data) {
+
+  //access the value of the 'data' object using the 'val()' method
+  console.log(data.val());
+
+}, function(error) {
+
+  console.log("Error: " + error.code);
+
+});
