fee7cd75fa5b0341a138939fac1e6ba2a446a2b6
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 20:52:13 -0500

    grab a reference to the first player in the 'players' that is a child of(nested inside) the 'players' collection

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -4,3 +4,7 @@
 
 //grab a reference to 'players', which will be used as the name to create a collection later
 var ref = db.ref('players');
+
+
+// grab a reference to the first player in the 'players' that is a child of(nested inside) the 'players' collection
+var firstPlayerRef = firebase.database().ref("/players/players").limitToFirst(1);
