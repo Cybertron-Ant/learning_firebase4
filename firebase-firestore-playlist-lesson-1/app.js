2e9f104897bc26d561033177b1b1a2fd0d76c7e5
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 19:38:36 -0500

    To order data by 'name'. I will see names in the alphabetic order.

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -8,3 +8,12 @@ var ref = db.ref('players');
 
 //grab a reference to the 'players' that is a child of(nested inside) the 'players' collection
 var playersRef = db.ref('/players/players');
+
+
+//To order data by 'name'. I will see names in the alphabetic order.
+playersRef.orderByChild("name").on("child_added", function(data) {
+
+  //access the value of the 'data' object using the 'val()' method, then access its 'name' property
+  console.log(data.val().name);
+
+});
