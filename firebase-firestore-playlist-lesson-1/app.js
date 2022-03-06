137365e43a6560a97764f9056d70b48ebece98fd
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-06 13:21:16 -0500

    use the 'set' method will write or replace data on a specified path

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -8,3 +8,13 @@ var ref = firebase.database().ref();
 
 //grab a reference to the 'ratings2' collection, will create it if it doesn't exist, and call the 'child' method on it
 var ratingsRef = ref.child("ratings2");
+
+
+//use the 'set' method will write or replace data on a specified path
+ratingsRef.set({
+   
+      Amanda: 11,
+      Bob: 8,
+      John: 10  
+   
+});//end set method
