f0d7751a758fc3e34fcfc3d43acbc7362d150daa
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 19:17:21 -0500

    attach a callback function for the 'value' event

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -8,3 +8,16 @@ var ref = db.ref('players');
 
 //grab a reference to the 'players' that is a child of(nested inside) the 'players' collection
 var playersRef = db.ref('/players/players');
+
+
+//attach a callback function for the 'value' event
+playersRef.on("value", function(data) {
+
+   //access the value of the 'data' object using the 'val()' method
+   console.log(data.val());
+
+}, function (error) {
+
+   console.log("Error: " + error.code);
+
+});
