a7b9caf77c13238165edb288b050b8ae62290324
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 20:58:14 -0500

    grab a reference to the last player in the 'players' that is a child of(nested inside) the 'players' collection

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -4,3 +4,7 @@
 
 //grab a reference to 'players', which will be used as the name to create a collection later
 var ref = db.ref('players');
+
+
+//grab a reference to the last player in the 'players' that is a child of(nested inside) the 'players' collection
+var lastPlayerRef = firebase.database().ref("/players/players").limitToFirst(1);
