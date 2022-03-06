589320f5b6a60f4e784dcd02b74b283708a660e0
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-06 11:53:25 -0500

    define variables to hold email and password

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -4,3 +4,8 @@
 
 //grab a reference to 'players', which will be used as the name to create a collection later
 //var ref = db.ref('players');
+
+
+//define variables to hold email and password
+var email = "myemail@email.com";
+var password = "mypassword";
