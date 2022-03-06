4a5a8e11eed79420b38d6cc932bdd0a07eec4e30
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-05 19:44:00 -0500

    chain the 'orderByKey' method

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -8,3 +8,12 @@ var ref = db.ref('players');
 
 //grab a reference to the 'players' that is a child of(nested inside) the 'players' collection
 var playersRef = db.ref('/players/players');
+
+
+// chain the 'orderByKey' method
+playersRef.orderByKey().on("child_added", function(data) {
+
+   //access the 'key' property from the 'data' object
+   console.log(data.key);
+
+});
