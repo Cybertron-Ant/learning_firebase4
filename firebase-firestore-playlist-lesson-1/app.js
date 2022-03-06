406605b8fb330f9afb48b99b1972eb35aab942dd
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-06 12:54:46 -0500

    grab a reference to the 'ratings2' collection, will create it if it doesn't exist, and call the 'child' method on it

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -4,3 +4,7 @@
 
 //grab a reference to 'players', which will be used as the name to create a collection later
 //var ref = db.ref('players');
+
+
+//grab a reference to the 'ratings2' collection, will create it if it doesn't exist, and call the 'child' method on it
+var ratingsRef = ref.child("/ratings2");
