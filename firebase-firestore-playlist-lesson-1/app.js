151cb4f6f7956a10339dd2f6201eb7fb822e2938
Cybertron-Ant <Mediainformationofficer@gmail.com>
2022-03-06 13:19:46 -0500

    grab a reference to the 'root' collection/directory, the database itself

--- firebase-firestore-playlist-lesson-1/app.js
+++ firebase-firestore-playlist-lesson-1/app.js
@@ -2,9 +2,9 @@
 
 
 
-//grab a reference to 'players', which will be used as the name to create a collection later
-//var ref = db.ref('players');
+//grab a reference to the 'root' collection/directory, the database itself
+var ref = firebase.database().ref();
 
 
 //grab a reference to the 'ratings2' collection, will create it if it doesn't exist, and call the 'child' method on it
-var ratingsRef = ref.child("/ratings2");
+var ratingsRef = ref.child("ratings2");
